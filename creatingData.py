b08a05f6e60cd42454b12298ef255a1d6d638413
ironmerc <sinhaakshat2012@gmail.com>
2024-06-09 22:51:44 +0530

    Initial Commit

---
+++ creatingData.py
@@ -0,0 +1,68 @@
+#NOTE: The functions and code snippets written in the file are used when required by calling them manually, you can modify the code to
+# automate the program
+
+import sounddevice as sd
+import librosa
+import numpy as np
+from scipy.io.wavfile import write
+import pandas as pd
+
+#records your voice n times, where n is input by user
+
+def record_audio():
+    fs = 44100
+    duration = 3
+
+    print("how many recordings?? ")
+    n = int(input())
+
+    for i in range(0, n+1):
+        print("recording started")
+
+        rec = sd.rec(int((duration * fs)), samplerate=fs, channels=1)
+
+        sd.wait()
+
+        print("recording stopped")
+
+        fileName = "C:\\Users\\sinha\\Desktop\\Voice-Biometric\\data\\akshat" + str(i) + ".wav"
+        write(filename=fileName, rate=fs, data=rec)
+
+
+#used in model.py / used to return 40 mfccs of a particular audio file.
+def extract_mfcc(file, n_mfcc=40):
+    audio, sr = librosa.load(file)
+
+    mfccs = np.mean(librosa.feature.mfcc(y=audio, sr=sr, n_mfcc=n_mfcc).T, axis=0)
+
+    return mfccs
+
+
+
+# Extract mfccs
+def createMfccCsv():
+    df = pd.DataFrame(columns=range(0,40))
+    for i in range(0, 21):
+        fileName = "C:\\Users\\sinha\\Desktop\\Voice-Biometric\\data\\akshat" + str(i) + ".wav"
+        mfccs = extract_mfcc(fileName, 40)
+        lst = list(mfccs)
+        df.loc[len(df)] = lst
+    df.to_csv("C:\\Users\\sinha\\Desktop\\Voice-Biometric\\data\\akshat\\akshat.csv") #dont forget to change the paths (there are literally many paths in the project)
+
+
+# Append all individual data to complete data.
+def appendIndividualToCompleteCSV():
+    df1 = pd.read_csv("C:\\Users\\sinha\\Desktop\\Voice-Biometric\\data\\complete_data.csv") #read completedata.csv
+    df2 = pd.read_csv("C:\\Users\\sinha\\Desktop\\Voice-Biometric\\data\\akshat\\akshat.csv") #read speaker_name.csv
+    df1 = df1._append(df2, ignore_index=True) #append df2 into df1
+    df1.to_csv("C:\\Users\\sinha\\Desktop\\Voice-Biometric\\data\\akshat\\complete_data.csv") # save df1 in complete_data.csv (!! Note that the csv file should not be open in excel, that will give error).
+
+
+#call this function to record audio files
+#record_audio();
+
+# call this function to create speaker_name.csv files of individual speakers
+#createMfccCsv();
+
+#call this function to append speaker_name.csv of individual speaker into completedata.csv
+appendIndividualToCompleteCSV()
---
+++ model.py
@@ -0,0 +1,108 @@
+from model_testing import verifyUser
+import sounddevice as sd
+from scipy.io.wavfile import write
+from creatingData import extract_mfcc
+from sklearn.naive_bayes import BernoulliNB
+import pandas as pd
+from sklearn.neural_network import MLPClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+import time
+import warnings
+import pyttsx3
+
+#setup voice engine of py text to speech
+engine = pyttsx3.init()
+voices = engine.getProperty("voices")
+engine.setProperty("voice", voices[1].id)
+engine.setProperty("rate", 200)
+
+
+def speaker_identifier():
+
+    fs = 44100
+    duration = 3
+    print("speak Hello when the recording starts")
+    engine.say("Speak Hello when the recording starts!")
+    engine.runAndWait()
+
+    time.sleep(0.1)
+    print("recording started")
+
+    rec = sd.rec(int((duration * fs)), samplerate=fs, channels=1)
+
+    sd.wait()
+
+    print("recording stopped")
+
+    file = "C:\\Users\\sinha\\Desktop\\Voice-Biometric\\data\\history\\last_try.wav"
+    #this will save the audio of the last person tried to access the software,
+    # will help later to identify the person
+    write(filename=file, rate=fs, data=rec)
+
+    mfcc = extract_mfcc(file, n_mfcc=40) #defined in creatingData.py
+
+    input = pd.DataFrame(columns=range(0, 40))
+
+    lst = list(mfcc)
+    input.loc[len(input)] = lst
+
+    df = pd.read_csv("C:/Users/sinha/Desktop/Voice-Biometric/data/complete_data.csv")  # target variable is boolean : 1 means sahil, 0 means unknown
+    
+    Y = df.dropna()
+    Y = df["speaker"]
+    X = df.drop(columns=["speaker", "Unnamed: 0"])
+
+
+
+    # NOTE: Below are 5 models i researched upon out of which i found MLP neural netowrks best and also optimized them
+    # to their highest accuracy through many iterations.
+
+    # MLPClassifier, working fair.
+    #Please optimize the classifier according to your requirements.
+    classifier = MLPClassifier(solver='adam', alpha=0.001,
+                               random_state=1, max_iter=500,
+                               hidden_layer_sizes=100, activation="logistic")
+
+    warnings.simplefilter("ignore")
+    classifier.fit(X, Y)
+
+    pred_mlp = classifier.predict(input)
+    return pred_mlp[0]
+
+
+    # random forest, average
+    # warnings.simplefilter("ignore")
+    # clf_forest = RandomForestClassifier(max_depth=100, random_state=1
+    #                                     , n_estimators=75, criterion="entropy",
+    #                                     max_features="auto")
+    # clf_forest.fit(X, Y)
+    # pred_rf = clf_forest.predict(input)
+    # return pred_rf[0]
+
+
+    # K nearest neighbours classifier, not working for true cases
+    # warnings.simplefilter("ignore")
+    # neigh = KNeighborsClassifier(n_neighbors=3)
+    # neigh.fit(X, Y)
+    # pred_knn = neigh.predict(input)
+    # return pred_knn[0]
+
+
+    # Naive Bayes gaussian, less accuracy
+    # warnings.simplefilter("ignore")
+    # gnb = GaussianNB()
+    # gnb.fit(X, Y)
+    # pred_gnb = gnb.predict(input)
+    # return pred_gnb[0]
+
+
+    # Naive bayes bernoulli, not working properly, rejected
+
+    # warnings.simplefilter("ignore")
+    # bnb = BernoulliNB()
+    # bnb.fit(X, Y)
+    # pred_bnb = bnb.predict(input)
+    # return pred_bnb[0]
